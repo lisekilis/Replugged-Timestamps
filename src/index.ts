ec38dd5e0d231288ba2914a7bb58a757dc6f43bf
lisekilis <lisekilisgewp@gmail.com>
2024-02-04 19:39:19 +0100

    The thing

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,46 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, common } from "replugged";
 
+const { messages } = common;
 const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
 
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
+function getTime(messageContent: string): string | null {
+  const time = messageContent.match(/([0-1][0-9]|2[0-3]):([0-5][0-9])|24:00/);
+  return time ? time[0] : null;
+}
+function getTimestamp(time: string): string {
+  const colonLocation = time.indexOf(":");
+  const currentDayTimestamp = Math.floor(Date.now() / 86400000) * 86400;
+  const timezoneOffset = new Date().getTimezoneOffset();
+  const seconds =
+    Number(time.slice(0, colonLocation)) * 3600 +
+    Number(time.slice(colonLocation + 1)) * 60 +
+    timezoneOffset * 60;
+  const timestamp = `<t:${(currentDayTimestamp + seconds).toString()}:t>`;
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
+  return timestamp;
 }
 
+function replaceTimestamp(orgContent: string, orgTime: string): string {
+  const newContent = `${orgContent.slice(0, orgContent.indexOf(orgTime))}${getTimestamp(orgTime)}${orgContent.slice(orgContent.indexOf(orgTime) + orgTime.length)}`;
+  const newTime = getTime(newContent);
+  if (newTime != null) {
+    return replaceTimestamp(newContent, newTime);
+  }
+  return newContent;
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function start(): Promise<void> {
+  inject.before(messages, "sendMessage", (_args) => {
+    const orgContent = _args[1].content;
+    const time = getTime(orgContent);
+    if (time != null) {
+      const newContent = replaceTimestamp(orgContent, time);
+      _args[1].content = newContent;
+    }
+    return _args;
+  });
+}
 export function stop(): void {
   inject.uninjectAll();
 }
