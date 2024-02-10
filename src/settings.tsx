9d3571f6296a2882cdf85869bfa7fa6955091b18
lisekilis <lisekilisgewp@gmail.com>
2024-02-10 02:31:26 +0100

    Dates + Full Rewrite

--- src/settings.tsx
+++ src/settings.tsx
@@ -1,32 +1,105 @@
 import { components, settings, util } from "replugged";
 
-const { Clickable, Category, Divider, Flex, SwitchItem, Text, Tooltip } = components;
+const { Clickable, Category, Divider, Flex, SwitchItem, SelectItem, Text, Tooltip } = components;
 
 const cfg = await settings.init("dev.lisekilis.RepluggedTimestamps");
-
+//TO DO: make the formating table look cool using flex
 export function Settings(): React.ReactElement {
   return (
     <>
-      <SwitchItem
-        {...{
-          ...util.useSetting(cfg, "prefix", false),
-        }}>
-        Required Prefix
-      </SwitchItem>
+      <SwitchItem {...util.useSetting(cfg, "prefix", true)}>Required Prefix</SwitchItem>
       <Category title="Formating">
-        <Text.H2 markdown>{"t-hh:mm <t:1692525600:t>"}</Text.H2>
-        <Divider />
-        <Text.H2 markdown>{"T-hh:mm:ss <t:1692525600:T>"}</Text.H2>
-        <Divider />
-        <Text.H2 markdown>{"d-hh:mm <t:1692525600:d>"}</Text.H2>
-        <Divider />
-        <Text.H2 markdown>{"D-hh:mm <t:1692525600:D>"}</Text.H2>
-        <Divider />
-        <Text.H2 markdown>{"f-hh:mm <t:1692525600:f>"}</Text.H2>
-        <Divider />
-        <Text.H2 markdown>{"F-hh:mm <t:1692525600:F>"}</Text.H2>
-        <Divider />
-        <Text.H2 markdown>{"R-hh:mm <t:1692525600:R>"}</Text.H2>
+        <table border={1}>
+          <tr>
+            <td>
+              <Text.H2 markdown>{"t-hh:mm"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:t>"}</Text.H2>
+            </td>
+          </tr>
+          <Divider />
+          <tr>
+            <td>
+              <Text.H2 markdown>{"T-hh:mm:ss"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:T>"}</Text.H2>
+            </td>
+          </tr>
+          <Divider />
+          <tr>
+            <td>
+              <Text.H2 markdown>{"d-hh:mm"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:d>"}</Text.H2>
+            </td>
+          </tr>
+          <Divider />
+          <tr>
+            <td>
+              <Text.H2 markdown>{"D-hh:mm"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:D>"}</Text.H2>
+            </td>
+          </tr>
+          <Divider />
+          <tr>
+            <td>
+              <Text.H2 markdown>{"f-hh:mm"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:f>"}</Text.H2>
+            </td>
+          </tr>
+          <Divider />
+          <tr>
+            <td>
+              <Text.H2 markdown>{"F-hh:mm"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:F>"}</Text.H2>
+            </td>
+          </tr>
+          <Divider />
+          <tr>
+            <td>
+              <Text.H2 markdown>{"R-hh:mm"}</Text.H2>
+            </td>
+            <td>
+              <Text.H2 markdown>{"<t:1692525600:R>"}</Text.H2>
+            </td>
+          </tr>
+        </table>
+      </Category>
+      <Category title="Date Format">
+        <SelectItem
+          {...util.useSetting(cfg, "format", "dmy")}
+          options={[
+            {
+              label: "DD/MM/YYYY",
+              value: "dmy",
+            },
+            {
+              label: "MM/DD/YYYY",
+              value: "mdy",
+            },
+            {
+              label: "YYYY/MM/DD",
+              value: "ymd",
+            },
+            {
+              label: "YYYY/DD/MM",
+              value: "ydm",
+            },
+          ]}>
+          Date format (separator doesn't matter)
+        </SelectItem>
+        <SwitchItem {...util.useSetting(cfg, "shortYear", false)}>
+          Use short Year (2 digits)
+        </SwitchItem>
       </Category>
       <Clickable
         onClick={() =>
